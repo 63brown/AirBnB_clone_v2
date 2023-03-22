dffe2ecf8c099949dc0baa2445627758a699203f
63brown <mutheemumbi01@gmail.com>
2023-03-22 14:21:11 +0000

    Task 9

--- models/place.py
+++ models/place.py
@@ -19,6 +19,7 @@ class Place(BaseModel, Base):
         price_by_night = Column(Integer, nullable=False, default=0)
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
+        reviews = relationship('Review', backref='place', cascade='all, delete, delete-orphan')
     else:
         city_id = ""
         user_id = ""
--- models/user.py
+++ models/user.py
@@ -16,6 +16,7 @@ class User(BaseModel, Base):
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship('Place',cascade='all, delete, delete-orphan',backref='user')
+        reviews = relationship('Review', backref='user', cascade='all, delete, delete-orphan')
     else:
         email = ""
         password = ""
