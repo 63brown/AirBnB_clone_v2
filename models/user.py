d1e23111e15e6e366849716d5166a6d228a2133d
Irvine Sunday <amugumbiirvine@gmail.com>
2023-03-22 20:34:13 +0300

    Update user.py

--- models/user.py
+++ models/user.py
@@ -25,6 +25,6 @@ class User(BaseModel, Base):
         first_name = ""
         last_name = ""
                               
-   def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
