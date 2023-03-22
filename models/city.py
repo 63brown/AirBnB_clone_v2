e98c89275185cdddf4b7dd0210012531045c2feb
Irvine Sunday <amugumbiirvine@gmail.com>
2023-03-22 21:05:05 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 """ City Module for HBNB project """
 import models
 from models.base_model import BaseModel, Base
@@ -9,6 +10,15 @@ from os import environ
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship('Place', cascade='all, delete', backref='cities')
+    if environ['HBNB_TYPE_STORAGE'] == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship('Place', cascade='all, delete', backref='cities')
+        
+    else:
+        state_id = ""
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
