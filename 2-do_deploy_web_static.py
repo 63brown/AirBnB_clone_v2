bcd51ac34cae9e04b0108a4ad30a8483a1c22ee3
63brown <mutheemumbi01@gmail.com>
2023-04-08 11:32:30 +0300

    deploy static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,53 +1,46 @@
 #!/usr/bin/python3
 """ Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy"""
-
-import os
-from os.path import exists
+import os.path
 from fabric.api import env, put, run
-
 
 env.hosts = ['100.26.177.16', '100.26.9.154']  # Replace with your own IP addresses
 
 
 def do_deploy(archive_path):
+    """Distributes an archive to a web server.
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
-    Distributes an archive to your web servers.
-    """
-    # Check if the archive file exists
-    if not exists(archive_path):
+    if os.path.isfile(archive_path) is False:
         return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-    # Get the name of the archive
-    archive_name = os.path.basename(archive_path)
-
-    # Define the remote paths
-    remote_archive = '/tmp/{}'.format(archive_name)
-    remote_folder = '/data/web_static/releases/{}'.format(archive_name.split('.')[0])
-
-    # Upload the archive to the remote server
-    put(archive_path, remote_archive)
-
-    # Create the folder to store the archive
-    run('sudo mkdir -p {}'.format(remote_folder))
-
-    # Uncompress the archive into the folder
-    run('sudo tar -xzf {} -C {}'
-        .format(remote_archive, remote_folder))
-
-    # Remove the archive from the remote server
-    run('sudo rm {}'.format(remote_archive))
-
-    # Move the contents of the folder to the parent folder
-    run('sudo mv {}/* {}'.format(remote_folder, remote_folder+'/..'))
-
-    # Remove the original folder
-    run('sudo rm -rf {}'.format(remote_folder))
-
-    # Remove the existing symbolic link
-    run('sudo rm -rf /data/web_static/current')
-
-    # Create a new symbolic link to the new version
-    run('sudo ln -s {} /data/web_static/current'
-        .format(remote_folder+'/'))
-
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
     return True
