d6aed6576fae608f1ef145b0e38e7145edd5d224
63brown <mutheemumbi01@gmail.com>
2023-04-08 11:15:16 +0300

    deploy static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,74 +1,64 @@
-from fabric.api import env, local
+from fabric.api import *
 from os.path import exists
 from datetime import datetime
 
-# Define the environment
-env.user = 'ubuntu'
+
 env.hosts = ['100.26.177.16', '100.26.9.154']  # Replace with your own IP addresses
 
-def do_pack():
-    """
-    Creates an archive of the web_static folder.
-    """
-    now = datetime.now()
-    local('mkdir -p versions')
-    file_path = 'versions/web_static_{}{}{}{}{}{}.tgz'.format(now.year,
-                                                              now.month,
-                                                              now.day,
-                                                              now.hour,
-                                                              now.minute,
-                                                              now.second)
-    local('tar -cvzf {} web_static'.format(file_path))
 
-    if exists(file_path):
-        return file_path
-    else:
-        return None
+@runs_once
+def do_pack():
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    cur_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        cur_time.year,
+        cur_time.month,
+        cur_time.day,
+        cur_time.hour,
+        cur_time.minute,
+        cur_time.second
+    )
+    try:
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        archize_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
+    except Exception:
+        output = None
+    return output
+
 
 def do_deploy(archive_path):
+    """Deploys the static files to the host servers.
+    Args:
+        archive_path (str): The path to the archived static files.
     """
-    Distributes an archive to your web servers.
-    """
-    # Check if the archive file exists
-    if not exists(archive_path):
+    if not os.path.exists(archive_path):
         return False
+    file_name = os.path.basename(archive_path)
+    folder_name = file_name.replace(".tgz", "")
+    folder_path = "/data/web_static/releases/{}/".format(folder_name)
+    success = False
+    try:
+        put(archive_path, "/tmp/{}".format(file_name))
+        run("mkdir -p {}".format(folder_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        run("rm -rf /tmp/{}".format(file_name))
+        run("mv {}web_static/* {}".format(folder_path, folder_path))
+        run("rm -rf {}web_static".format(folder_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(folder_path))
+        print('New version is now LIVE!')
+        success = True
+    except Exception:
+        success = False
+    return success
 
-    # Get the name of the archive
-    archive_name = archive_path.split('/')[-1]
-
-    # Define the remote paths
-    remote_archive = '/tmp/{}'.format(archive_name)
-    remote_folder = '/data/web_static/releases/{}'.format(
-        archive_name.replace('.tgz', ''))
-
-    # Upload the archive to the remote server
-    put(archive_path, remote_archive)
-
-    # Create the folder to store the archive
-    run('sudo mkdir -p {}'.format(remote_folder))
-
-    # Uncompress the archive into the folder
-    run('sudo tar -xzf {} -C {}'
-        .format(remote_archive, remote_folder))
-
-    run('sudo rm {}'.format(remote_archive))
-
-    run('sudo mv {}/* {}'.format(remote_folder, remote_folder+'/..'))
-    run('sudo rm -rf {}'.format(remote_folder))
-    run('sudo rm -rf /data/web_static/current')
-    run('sudo ln -s {} /data/web_static/current'
-        .format(remote_folder+'/'))
-
-    return True
 
 def deploy():
+    """Archives and deploys the static files to the host servers.
     """
-    Creates and distributes an archive to your web servers.
-    """
-    # Create the archive
     archive_path = do_pack()
-    if archive_path is None:
-        return False
-
-    # Deploy the archive
-    return do_deploy(archive_path)
+    return do_deploy(archive_path) if archive_path else False
